4c77f634dd8a588fe0050f694398d096ed638d9b
MatiGoldman <goldman_matias@hotmail.com>
2020-07-26 22:39:05 +0300

    Add project structure

---
+++ src/test/java/com/checkpoint/CpdirAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpdirAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
