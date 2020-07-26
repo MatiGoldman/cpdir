d8fb2a60a1afbe7671523f3530d120f276357ae3
MatiGoldman <goldman_matias@hotmail.com>
2020-07-26 22:49:48 +0300

    Add entity Employee

---
+++ src/main/java/com/checkpoint/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.checkpoint.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String teamName;
+    private String positionName;
+    private Long idManager;
+    private int phone;
+}
